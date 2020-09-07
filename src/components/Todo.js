a19e7c60f807fdb6dfc7657ea216db337d184a0a
DeAdFrOnt <semenukmaks@gmail.com>
2020-09-07 18:59:13 +0300

    feat: complete btn

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,10 +4,24 @@ function Todo({ todo, todos, setTodos }) {
   function deleteHandler() {
     setTodos(todos.filter((el) => el.id !== todo.id));
   }
+  function completeHandler() {
+    setTodos(
+      todos.map((el) => {
+        if (el.id === todo.id) {
+          return {
+            ...el,
+            completed: !el.completed,
+          };
+        }
+        return el;
+      })
+    );
+  }
+
   return (
     <div className="todo">
       <li className="todo-item">{todo.title}</li>
-      <button className="complete-btn">
+      <button onClick={completeHandler} className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
       <button onClick={deleteHandler} className="trash-btn">
