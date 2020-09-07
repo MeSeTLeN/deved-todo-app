fa5539115a426e143a8f32a274da03f89b2ef6f0
DeAdFrOnt <semenukmaks@gmail.com>
2020-09-07 20:01:58 +0300

    add: filter todo func

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
@@ -7,13 +7,34 @@ function App() {
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
   const [status, setStatus] = useState("all");
+  const [selectedTodos, setSelectedTodos] = useState([]);
+
+  useEffect(() => {
+    selectedTodosHandler();
+  }, [todos, status]);
+
+  function selectedTodosHandler() {
+    switch (status) {
+      case "completed":
+        setSelectedTodos(todos.filter((el) => el.completed === true));
+        break;
+
+      case "uncompleted":
+        setSelectedTodos(todos.filter((el) => el.completed === false));
+        break;
+
+      default:
+        setSelectedTodos(todos);
+        break;
+    }
+  }
 
   return (
     <div className="App">
       <header>
         <h1>Ed's Todo List</h1>
       </header>
-      <button onClick={() => console.log(inputText)}>inputTextConsole</button>
+      <button onClick={() => console.log(selectedTodos)}>selectedTodos</button>
       <Form
         inputText={inputText}
         setInputText={setInputText}
