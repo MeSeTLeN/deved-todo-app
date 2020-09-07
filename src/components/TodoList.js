6ac5cbd20017fa8cfa5082158171ce990be34c17
DeAdFrOnt <semenukmaks@gmail.com>
2020-09-07 18:03:29 +0300

    feat: todos map

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-export default function TodoList() {
+export default function TodoList({ todos }) {
   return (
     <div className="todo-continer">
-      <ul className="todo-list"></ul>
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title} </li>
+        ))}
+      </ul>
     </div>
   );
 }
