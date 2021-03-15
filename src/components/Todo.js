ec47b8d79acd118e25e9998eb66a3858b506b1c0
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-15 12:08:58 +0200

    feat: Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,33 @@
 import React from "react";
 
-export default function Todo({ todo }) {
+export default function Todo({ todo, todos, setTodos }) {
+  function deleteHandler() {
+    setTodos(todos.filter((elem) => elem.id !== todo.id));
+  }
+
+  function completeHandler() {
+    setTodos(
+      todos.map((elem) => {
+        if (elem.id === todo.id) {
+          return {
+            ...elem,
+            completed: !elem.completed,
+          };
+        }
+        return elem;
+      })
+    );
+  }
+
   return (
     <div className="todo">
-      <li className="todo-item">{todo.text}</li>
-      <button className="complete-btn">
+      <li className={`todo-item ${todo.completed ? "completed" : ""}`}>
+        {todo.text}
+      </li>
+      <button onClick={completeHandler} className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
-      <button className="trash-btn">
+      <button onClick={deleteHandler} className="trash-btn">
         <i className="fas fa-trash"></i>
       </button>
     </div>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Todo from "./Todo";
 
-export default function TodoList({ todos }) {
+export default function TodoList({ todos, setTodos }) {
   return (
     <div className="todo-container">
       <ul className="todo-list">
         {todos.map((todo, i) => {
-          return <Todo todo={todo} key={i} />;
+          return <Todo todo={todo} key={i} todos={todos} setTodos={setTodos} />;
         })}
       </ul>
     </div>
