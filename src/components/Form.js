25c2db4d46eb75f1d3d14c0cb01a74230a391f20
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-16 16:46:34 +0200

    feat: add todo

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Form() {
+export default function Form({ todos, setTodos }) {
+  const [inputValue, setInputValue] = useState("");
+
+  function inputValueHandler(e) {
+    setInputValue(e.target.value);
+  }
+
+  function submitTodoHandler(e) {
+    e.preventDefault();
+
+    setTodos([
+      ...todos,
+      { text: inputValue, completed: false, id: Math.random() * 1000 },
+    ]);
+
+    setInputValue("");
+  }
+
   return (
     <form>
-      <input type="text" className="todo-input" />
-      <button className="todo-button" type="submit">
+      <input
+        value={inputValue}
+        onChange={inputValueHandler}
+        type="text"
+        className="todo-input"
+      />
+      <button onClick={submitTodoHandler} className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
       <div className="select">
