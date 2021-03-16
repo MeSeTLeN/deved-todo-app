a83fde65379f869c28f79053319628ab7c5784e2
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-16 16:56:47 +0200

    feat: todo delete

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -15,6 +15,10 @@ export default function Todo({ todo, todos, setTodos }) {
     );
   }
 
+  function deleteTodoHandler(params) {
+    setTodos(todos.filter((el) => el.id !== todo.id));
+  }
+
   return (
     <div className="todo">
       <li className={`todo-item ${todo.completed ? "completed" : null}`}>
@@ -23,7 +27,7 @@ export default function Todo({ todo, todos, setTodos }) {
       <button onClick={completeTodoHandler} className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
-      <button className="trash-btn">
+      <button onClick={deleteTodoHandler} className="trash-btn">
         <i className="fas fa-trash"></i>
       </button>
     </div>
